25c54cfad1523735976e04763ab63e0773553c94
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-05-30 10:59:33 -0500

    server stuff

--- server/index.js
+++ server/index.js
@@ -3,3 +3,19 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+    .connect(MONGOURL)
+    .then(()=>{
+        console.log("DB connected successfully");
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on port:${PORT}`)
+        });
+    })
+    .catch((error)=>console.log(error));
