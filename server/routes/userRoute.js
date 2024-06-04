b6434968e51aeb0c23a1c4ef44be5e91d9ec1767
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-06-04 14:40:45 -0500

    get

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import {create} from '../controller/userController.js';
+import {create, getAllUsers} from '../controller/userController.js';
 
 const route = express.Router();
 
+//Post route
 route.post("/user", create)
+//Get route
+route.get("/users", getAllUsers)
+
 
 export default route;
 
