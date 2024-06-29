c9838c16b47e7440157f8810161239b16dff7fa7
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-06-29 15:22:10 -0500

    installed react-hot-toast

--- client/src/addUser/AddUser.jsx
+++ client/src/addUser/AddUser.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './adduser.css';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import axios from "axios";
 
 const AddUser = () => {
+    const users = {
+      name:"",
+      email:"",
+      address:"",
+    };
+
+    const[user, setUser] = useState(users)
+    const navigate = useNavigate();
+
+    const inputHandler = (e) =>{
+      const {name, value}=e.target
+      console.log(name, value);
+      setUser({...user, [name]:value})
+    };
+
+// function to submit the form.
+const submitForm = async(e)=>{
+  e.preventDefault();
+  await axios.post("http://localhost:8000/api/user", user)
+  .then((response)=>{
+    console.log("User created successfully.")
+    navigate("/");
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+}
+
   return (
     <div className='addUser'>
 
@@ -13,12 +42,13 @@ const AddUser = () => {
          
         </Link>
 
-       <form className='addUserForm'>
+       <form className='addUserForm' onSubmit={submitForm}>
         <div className='inputGroup'>
           <label htmlFor='name'>Name:</label>
           <input 
           type='text'
           id='name'
+          onChange={inputHandler}
           name='name'
           autoComplete='off'
           placeholder='Enter your name'
@@ -29,7 +59,8 @@ const AddUser = () => {
           <input 
           type='text'
           id='email'
-          email='email'
+          onChange={inputHandler}
+          name='email'
           autoComplete='off'
           placeholder='Enter your email'
           />
@@ -39,14 +70,15 @@ const AddUser = () => {
           <input 
           type='text'
           id='address'
-          address='address'
+          onChange={inputHandler}
+          name='address'
           autoComplete='off'
           placeholder='Enter your address'
           />
           </div>
           {/* Let's add a submit button! */}
           <div className='inputGroup'>
-          <button type="button" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-primary">Submit</button>
           </div>
 
        </form>
